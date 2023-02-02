ae9e27b0178e0d00e5751109c5170a3f0366f1e7
getianao <getianao@gmail.com>
2023-02-02 12:05:06 +0800

    update

--- myplot.py
+++ myplot.py
@@ -9,10 +9,11 @@ import seaborn as sns
 
 
 def bar(apps, configs, values, ylabel: str, 
-        filename=None, groups=True, title=None, plotSize=(16, 4), ylim = None, 
+        filename=None, groups=True, groupsInterval = 0.10, title=None, plotSize=(16, 4), ylim = None, 
         plotAverage=True, labelAverage=True, averageXlabel = "Mean", averageFunc=np.mean, decimals=1,
         colorPalette=sns.color_palette("mako", 25), 
         edgecolor="black", showXAxis=True, legendPosition='best'):
+   averageHeightDict = {}
    maxHeight = float('-inf')
    minHeight = float('inf')
    opacity = 0.75
@@ -23,7 +24,7 @@ def bar(apps, configs, values, ylabel: str,
    # print(apps_index)
 
    if groups:
-      width = 1.0/(len(apps)+1.0)
+      width = (1.0 - groupsInterval)/(len(apps))
    else:
       width = 0.5
 
@@ -40,18 +41,18 @@ def bar(apps, configs, values, ylabel: str,
          plt.bar(np.array([len(apps)])+configId*width, [averageFunc(ys)], width*1.25,
                  alpha=min(opacity*1.20, 1.0), color=colorPalette[configId],
                  edgecolor=(edgecolor if edgecolor else colorPalette[configId]))
-         plt.axvline((len(apps)) - width/2.0, color='grey',  linestyle='dashed', linewidth=1)
          
-         height = averageFunc(ys) + 0.1
+         height = averageFunc(ys) + 0.3
          # print(ys)
          if ylim is not None and height > ylim[1]:
             height = ylim[1]+0.1
          avg = np.round(averageFunc(ys), decimals=decimals)
          label = format(avg, "."+str(decimals)+"f")
-         # print(ys, averageFunc(ys))
+         averageHeightDict[configs[configId]] = label
          if labelAverage:
             plt.text(np.array([len(apps)])+configId*width, height, label, fontsize=5,
                      ha='center', va='bottom', rotation=90)
+   plt.axvline((len(apps)) - groupsInterval, color='grey',  linestyle='dashed', linewidth=1)
 
    box = ax.get_position()
    ax.set_position([box.x0, box.y0*1.5, box.width, box.height])
@@ -59,16 +60,20 @@ def bar(apps, configs, values, ylabel: str,
    ax.xaxis.grid(False)  # horizontal grid
    
    ax.set_yscale('log', base=2)
-   ax.set_yticks([0.2, 1, 2, 4, 8, 16, 32, 64, 128], minor=False)
-   if ylim is not None:
-      plt.ylim(ylim[0], ylim[1])
-      # plt.ylim(0, 10.5)
-      # plt.ylim(-1, 6.25)
-   else:
-      if minHeight >= 0:
-         plt.ylim(0, maxHeight*1.1)
-      else:
-         plt.ylim(minHeight*1.1, maxHeight*1.1)
+   ax.set_yticks([0.25, 0.5, 1, 2, 4, 8, 16, 32, 64, 128], minor=False)
+   formatter = matplotlib.ticker.LogFormatter(base=2, labelOnlyBase=False)
+   # formatter.set_scientific(True) 
+   # formatter.set_powerlimits((0,8)) 
+   ax.yaxis.set_major_formatter(formatter)
+   # if ylim is not None:
+   #    plt.ylim(ylim[0], ylim[1])
+   #    # plt.ylim(0, 10.5)
+   #    # plt.ylim(-1, 6.25)
+   # else:
+   #    if minHeight >= 0:
+   #       plt.ylim(0, maxHeight*1.1)
+   #    else:
+   #       plt.ylim(minHeight*1.1, maxHeight*1.1)
 
    plt.ylabel(ylabel, fontsize=14)
 
@@ -88,6 +93,8 @@ def bar(apps, configs, values, ylabel: str,
          plt.xticks(apps_index, entries, rotation=30, ha='right')
       else:
          plt.xticks([], [])
+         
+   plt.margins(x=0.01)
 
 
    # legend
@@ -110,3 +117,7 @@ def bar(apps, configs, values, ylabel: str,
       plt.show()
    plt.clf()
    plt.close('all')
+   
+   print("maxHeight", "%.2f" % maxHeight)
+   print("minHeight", "%.2f" % minHeight)
+   print("averageHeightDict", averageHeightDict)
